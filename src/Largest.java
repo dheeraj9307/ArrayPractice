440eb343fa2b1375ee94ab566103569d2d15f429
Dheeraj Patel <dheeraj162001@gmail.com>
2023-04-28 14:07:46 +0530

    Largest_element

---
+++ src/Largest.java
@@ -0,0 +1,20 @@
+// FIND THE LARGEST ELEMENT IN THE ARRAY
+public class Largest {
+
+    static int getlargest(int[]arr,int n) {
+        int largest = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[largest])
+                largest = i;
+            }
+            return largest;
+        }
+
+
+    public static void main(String[] args) {
+        int[]arr={1,2,4,56,8};
+        int n= arr.length;
+        System.out.println(getlargest(arr,n));
+    }
+}
